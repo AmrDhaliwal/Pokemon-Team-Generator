3f1b36cc35b5faa98fc9b0555b4bd7d340dfceb7
Amrit Dhaliwal <amritdhaliwalz@gmail.com>
2023-09-23 23:43:36 +0000

    Added button to App.tsx In App.tsx added a simple button to test extension in the browser.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
 function App() {
   return (
     <>
       <div>
         <h1>Random Pokemon Team Generator</h1>
+        <MyButton />
       </div>
     </>
   )
